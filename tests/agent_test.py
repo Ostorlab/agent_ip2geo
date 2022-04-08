f35431f0cd012c11341b506652e7c7d20d42972c
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-04-08 15:10:22 +0000

    Unittest for the ip2geoAgent.

---
+++ tests/agent_test.py
@@ -0,0 +1,86 @@
+"""Unittests for the Ip2Geo Agent."""
+import re
+import pytest
+
+from ostorlab.agent import message
+
+
+def testAgentIp2Geo_whenLocatesIpAddress_emitsBackFindings(ip2geo_agent, agent_mock, agent_persist_mock, requests_mock):
+    """Unittest for emitting back the geolocation details found by the Ip2Geo agent."""
+    del agent_persist_mock
+    matcher = re.compile('http://ip-api.com/json/')
+    requests_mock.get(
+        matcher,
+        json={
+            'query': '8.8.8.8',
+            'status': 'success',
+            'country': 'Canada',
+            'countryCode': 'CA',
+            'lat': 45.4995,
+            'lon': -73.5848,
+            'timezone': 'America/Toronto'
+        },
+        status_code=200
+    )
+
+    msg = message.Message.from_data(selector='v3.asset.ip.v4', data={'host': '8.8.8.8', 'version':4})
+    ip2geo_agent.process(msg)
+
+    assert len(agent_mock) == 1
+    assert agent_mock[0].selector =='v3.asset.ip.v4.geolocation'
+    assert agent_mock[0].data['longitude'] ==  pytest.approx(-73.5848)
+    assert 'country_code' in agent_mock[0].data and agent_mock[0].data['country_code'] == 'CA'
+
+
+def testAgentIp2Geo_whenIpAddressIsInvalid_shouldSkip(ip2geo_agent,
+                                                      agent_mock,
+                                                      agent_persist_mock,
+                                                      requests_mock):
+    """Unittest for Ip2Geo Agent, when it receives an invalid ip address, the agent should skip it."""
+    del agent_persist_mock
+    matcher = re.compile('http://ip-api.com/json/')
+    requests_mock.get(
+        matcher,
+        json={
+            'query': '8.8.',
+            'status': 'fail',
+            'message': 'query is in wrong format.'
+        },
+        status_code=200
+    )
+
+    msg = message.Message.from_data(selector='v3.asset.ip.v4', data={'host': '8.8.', 'version':4})
+    ip2geo_agent.process(msg)
+
+    assert len(agent_mock) == 0
+
+
+def testAgentIp2Geo_whenIpAddressHasAlreadyBeenProcessed_shouldSkip(ip2geo_agent,
+                                                                    agent_mock,
+                                                                    agent_persist_mock,
+                                                                    requests_mock):
+    """Unittest for Ip2Geo Agent, when it receives an ip address that has already been processed,
+       the agent should skip it.
+    """
+
+    del agent_persist_mock
+    matcher = re.compile('http://ip-api.com/json/')
+    requests_mock.get(
+        matcher,
+        json={
+            'query': '8.8.8.8',
+            'status': 'success',
+            'country': 'Canada',
+            'countryCode': 'CA',
+            'lat': 45.4995,
+            'lon': -73.5848,
+            'timezone': 'America/Toronto'
+        },
+        status_code=200
+    )
+
+    msg = message.Message.from_data(selector='v3.asset.ip.v4', data={'host': '8.8.', 'version':4})
+    ip2geo_agent.process(msg)
+    ip2geo_agent.process(msg)
+
+    assert len(agent_mock) == 1
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,24 @@
-"""
-    Dummy conftest.py for template_agent.
+"""Pytest fixtures for the Ip2Geo agent"""
 
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    - https://docs.pytest.org/en/stable/fixture.html
-    - https://docs.pytest.org/en/stable/writing_plugins.html
-"""
+import pytest
+import pathlib
 
-# import pytest
+from ostorlab.agent import definitions as agent_definitions
+from ostorlab.runtimes import definitions as runtime_definitions
+
+from agent import ip2geo_agent
+
+@pytest.fixture(scope='function', name='ip2geo_agent')
+def  fixture_io2geo_agent():
+    with (pathlib.Path(__file__).parent.parent / 'ostorlab.yaml').open() as yaml_o:
+        definition = agent_definitions.AgentDefinition.from_yaml(yaml_o)
+        settings = runtime_definitions.AgentSettings(
+            key='agent/ostorlab/subfinder',
+            bus_url='NA',
+            bus_exchange_topic='NA',
+            args=[],
+            healthcheck_port=5301,
+            redis_url='redis://guest:guest@localhost:6379')
+
+        agent = ip2geo_agent.Ip2GeoAgent(definition, settings)
+        return agent
