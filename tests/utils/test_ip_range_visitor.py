c987740dc4534d8457e056ea7277c4705da95f17
Najib RAIHAN <108728872+najibraihan@users.noreply.github.com>
2022-08-27 15:54:33 +0100

    Update tests/utils/test_ip_range_visitor.py
    Co-authored-by: Alaeddine Mesbahi <alaeddine.mesbahi@ostorlab.dev>

--- tests/utils/test_ip_range_visitor.py
+++ tests/utils/test_ip_range_visitor.py
@@ -9,7 +9,6 @@ def testVistor_withMatchingIPAndMaskRecieved_returnsLocations():
     results = []
     for result in ip_range_visitor.dichotomy_ip_network_visit(ipaddress.ip_network('8.8.8.0/22'),
                     ip_range_visitor.is_first_last_ip_same_geolocation):
-        print(result)
         results.append(result)
 
     assert all(len(result) == 3 and result[0] != result[1] for result in results)
