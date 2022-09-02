31eaffabc220084c31c1b154fc65ac0d85de4c74
amine <amine.mesbahi@ostorlab.dev>
2022-09-02 19:16:00 +0200

    Fix host bits

--- tests/agent_test.py
+++ tests/agent_test.py
@@ -62,3 +62,36 @@ def testAgentIp2Geo_whenIpAddressHasAlreadyBeenProcessed_shouldSkip(ip2geo_agent
 
     assert len(agent_mock) == 256
     assert mocker is not None
+
+
+def testAgentIp2Geo_whenIpAddressHasHostBits_shouldSkip(ip2geo_agent,
+                                                                    mocker,
+                                                                    agent_mock,
+                                                                    agent_persist_mock,
+                                                                    requests_mock):
+    """Unittest for Ip2Geo Agent, when it receives an ip address that has already been processed,
+       the agent should skip it.
+    """
+
+    del agent_persist_mock
+    matcher = re.compile('http://ip-api.com/json/')
+    requests_mock.get(
+        matcher,
+        json={
+            'query': '8.8.8.8',
+            'status': 'success',
+            'country': 'Canada',
+            'countryCode': 'CA',
+            'lat': 45.4995,
+            'lon': -73.5848,
+            'timezone': 'America/Toronto'
+        },
+        status_code=200
+    )
+
+    msg = message.Message.from_data(selector='v3.asset.ip.v4',
+                                    data={'host': '8.8.8.8', 'mask': '24', 'version': 4})
+    ip2geo_agent.process(msg)
+
+    assert len(agent_mock) == 256
+    assert mocker is not None
