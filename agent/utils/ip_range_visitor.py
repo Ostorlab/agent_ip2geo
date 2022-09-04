f9a844395ea632d93a9bd295c0f23602c2ef06e4
amine <amine.mesbahi@ostorlab.dev>
2022-09-04 17:43:12 +0200

    Fix errors mypy

--- agent/utils/ip_range_visitor.py
+++ agent/utils/ip_range_visitor.py
@@ -1,6 +1,6 @@
 """ Module Responsible for sending ip range geolocation"""
 import ipaddress
-from typing import Callable, Tuple, Any
+from typing import Callable, Tuple, Any, List
 
 from agent import ip2geo
 from agent.ip2geo import logger
@@ -27,7 +27,7 @@ class IpRangeVisitor:
         if should_continue is False:
             return
 
-        subnets = list(ip_network.subnets())
+        subnets: List[ipaddress.IPv4Network, ipaddress.IPv6Network] = list(ip_network.subnets())
 
         if len(subnets) == 1:
             # reached the last block.
